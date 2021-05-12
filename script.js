0c0b5020445a40c36ae11c4dfa56eb717943d62e
Arjun Sarath <arjunsarath7@gmail.com>
2021-05-12 11:56:04 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+function displaybox(num)
+{
+    dis.value += num;
+}
+
+function evaluateExpression()
+{
+    let val=dis.value
+    let result=eval(val)
+    dis.value=result
+}
+
+function clearScreen()
+{
+    dis.value=""
+}
+
+function backspace()
+{
+    dis.value=dis.value.slice(0,-1)
+}
